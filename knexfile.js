7aabbaf1dcc60240e11953b13af2727a5d7c9b3f
yilmaz1998 <snn.ylmz9804@gmail.com>
2025-10-06 21:04:46 -0400

    Fix production

--- knexfile.js
+++ knexfile.js
@@ -1,25 +1,29 @@
 require('dotenv').config();
 
 module.exports = {
-    development: {
-      client: 'postgresql',
-      connection: {
-        host: process.env.DB_HOST,
-        database: process.env.DB_NAME,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD
-      },
-      migrations: {
-        directory: './migrations'
-      },
-      seeds: {
-        directory: './seeds'
-      },
-      production: {
-        client: 'postgresql',
-        connection: process.env.DATABASE_URL + '?sslmode=require',
-        migrations: { directory: './migrations' },
-        seeds: { directory: './seeds' }
-      }
+  development: {
+    client: 'postgresql',
+    connection: {
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD
+    },
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
     }
-  };  
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?sslmode=require',
+    migrations: {
+      directory: './migrations'
+    },
+    seeds: {
+      directory: './seeds'
+    }
+  }
+};
